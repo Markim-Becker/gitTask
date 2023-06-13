e4659e5269c7b707e52e27babdc7fba44208ebc8
Markim-Becker <markimbecker47@gmail.com>
2023-06-13 13:34:49 +0200

    adding comments aswell as changing the name to python-hellowolrd

--- helloWolrd.py
+++ helloWolrd.py
@@ -1,1 +1,7 @@
-print('Git is awesome!')
+# changed the code to take input and then print that input
+user_string = input('\nPlease enter a sentance: ')
+
+print('\n',user_string)
+
+
+# bla blah blah blah
